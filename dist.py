19a10916cacd2ed2ae8c052942ec5d807890a974
avarvus <a.varvus@ya.ru>
2022-11-16 12:57:05 +0300

    calculate distance

---
+++ dist.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+DIST_ONE_M = 630
+
+tresh_high = (150,150,255)
+tresh_low = (0, 0, 0)
+
+photo = cv.imread('./img/cross.jpg')
+cv.imshow('orig', photo)
+only_object = cv.inRange(photo, tresh_low, tresh_high)
+moment = cv.moments(only_object, 1)
+area = int(moment['m00'])
+print(area / DIST_ONE_M * 1)
+cv.waitKey(0)
